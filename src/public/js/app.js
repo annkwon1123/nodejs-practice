1cafb28bd5f504081dd22973b913dff91218327b
Soyeon <annkwon1123@gmail.com>
2024-06-13 12:19:45 +0900

    socket

---
+++ src/public/js/app.js
@@ -0,0 +1,19 @@
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen() {
+    console.log("✅ Connected to Server");
+}
+
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+    console.log("Just got this: ", message.data);
+})
+
+socket.addEventListener("close", () => {
+    console.log("❎ Disconnected from Server");
+})
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000)
---
+++ src/server.js
@@ -0,0 +1,34 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req,res) => res.render("home"));
+app.get("/*", (req,res) => res.render("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+    console.log("❎ Disconnected from Browser");
+}
+
+function onSocketMessage(message) {
+    console.log(Buffer.from(message, "base64").toString("utf-8"));
+}
+
+wss.on("connection", (socket) => {
+    
+    console.log("✅ Connected to Browser");
+    socket.on("close", onSocketClose);
+    socket.on("message", onSocketMessage);
+    socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
